6e188c2ee929bc2e8b3198c489d86f4a7806a6e6
BooM04 <boom031@gmail.com>
2015-01-09 19:37:33 +0300

    moved modules to utilities

---
+++ utilities/Relays.cpp
@@ -0,0 +1,29 @@
+#include "Arduino.h"
+#include "Relays.h"
+
+
+Relay::Relay(byte pin)
+{
+count=1;
+
+Relay_arr[count].pin_id=pin;
+pinMode(Relay_arr[count].pin_id,  OUTPUT);
+}
+void Relay::addOne(byte pin)
+{
+count++; 
+Relay_arr[count].pin_id=pin;
+pinMode(Relay_arr[count].pin_id,  OUTPUT);
+}
+void Relay::setState (byte num,bool state)
+{
+ Relay_arr[num].state=state; //here you can put your own relay setState realisation 
+}
+//void  Relay::setName (byte num,)
+//{
+// Relay_arr[num].state=state; 
+//}
+bool Relay::getState  (byte num)
+{
+Serial.print(Relay_arr[num].state); //here you can put your own relay getState realisation 
+}
